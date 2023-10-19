af0d675b968ac02b48614d94e3d5d71987152641
dDevusS <geckahilden@gmail.com>
2023-10-18 21:58:55 -0400

    Add main menu

--- src/Game.java
+++ src/Game.java
@@ -1,11 +1,28 @@
+import game_processing.UserAction;
 
 public class Game {
 
 	public static void main(String[] args) {
-		
+		launchMainMenu();
 	}
 	
-	private static void launchMainMenu() {}
+	private static void launchMainMenu() {
+		System.out.println("""
+				BomberMan.
+			------------------
+			Start new game (press s)
+			Exit (press e)
+				""");
+		
+		switch (UserAction.input.nextLine().toLowerCase()) {
+		//case "1": startNewGame(); break;
+		case "s": startNewGame(); break;
+		//case "2": startNewGame(); break;
+		case "e": System.exit(0); break;
+		default:
+			System.out.println("Uncorrect command.");
+		}
+	}
 	
 	private static void startNewGame() {}
 	
--- src/game_processing/UserAction.java
+++ src/game_processing/UserAction.java
@@ -1,6 +1,10 @@
 package game_processing;
 
+import java.util.Scanner;
+
 public class UserAction {
+	
+	public static Scanner input = new Scanner(System.in);
 	
 	public void makeTurn() {}
 	
