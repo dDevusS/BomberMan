1057a7ee9f99206624da1f26b01a9e4509d21f9c
dDevusS <geckahilden@gmail.com>
2023-10-21 11:27:23 -0400

    Fix constructor GameField: added counterBobms++

--- src/gameEntity/GameField.java
+++ src/gameEntity/GameField.java
@@ -5,27 +5,27 @@ import java.util.Iterator;
 import java.util.Random;
 
 public class GameField {
-	
+
 	private HashMap<Coordinate, Cell> gameField = new HashMap();
-	
+
 	public GameField(int columns, int rows, int quantityOfBombs) {
 		Random random = new Random();
 		int counterBombs = 0;
-		
+
 		while (counterBombs < quantityOfBombs) {
 			int column = random.nextInt(columns);
 			int row = random.nextInt(rows);
 			Coordinate randomCoordinate = new Coordinate(column, row);
-			
+
 			if (gameField.get(randomCoordinate) == null || !gameField.get(randomCoordinate).isBomb()) {
 				gameField.put(randomCoordinate, new Cell(column, row, 9));
-				
-				for (int y = 1; y >= -1; y-- ) {
+				counterBombs++;
+
+				for (int y = 1; y >= -1; y--) {
 					for (int x = -1; x <= 1; x++) {
 						if (gameField.get(new Coordinate(column + y, row + x)) == null) {
-							gameField.put(new Coordinate(column + y, row + x), new Cell(column + y, row +x, 1));
-						}
-						else if (!gameField.get(new Coordinate(column + y, row + x)).isBomb()) {
+							gameField.put(new Coordinate(column + y, row + x), new Cell(column + y, row + x, 1));
+						} else if (!gameField.get(new Coordinate(column + y, row + x)).isBomb()) {
 							gameField.get(new Coordinate(column + y, row + x)).plusStep();
 						}
 					}
