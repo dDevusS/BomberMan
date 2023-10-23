89605a1e38614eb2fab70db38140209a1c3b1c90
dDevusS <geckahilden@gmail.com>
2023-10-23 14:01:47 -0400

    Changed scale of Field

--- src/gameProcessing/RenderingField.java
+++ src/gameProcessing/RenderingField.java
@@ -17,16 +17,16 @@ public class RenderingField {
 					System.out.print("   ");
 				}
 				else if (y == 0) {
-					System.out.print("---");
+					System.out.print("----");
 					if (x == game.getRows()) System.out.print("\n");
 				}	
 				else if (y == -1) {
 					if (x < 10) {
-						System.out.print(" " + x + " ");
+						System.out.print("  " + x + " ");
 						if (x == game.getRows()) System.out.print("\n");
 					}
 					else {
-						System.out.print(x + " ");
+						System.out.print(" "+ x + " ");
 						if (x == game.getRows()) System.out.print("\n");
 					}
 				}
@@ -40,10 +40,10 @@ public class RenderingField {
 				}
 				else {
 					if (gameField.get(new Coordinate(y, x)) == null) {
-						System.out.print(" 0 ");
+						System.out.print("  0 ");
 					}
 					else {
-						System.out.print(" " + gameField.get(new Coordinate(y, x)).getStepsFromBomb() + " ");
+						System.out.print("  " + gameField.get(new Coordinate(y, x)).getStepsFromBomb() + " ");
 					}
 				}
 			}
