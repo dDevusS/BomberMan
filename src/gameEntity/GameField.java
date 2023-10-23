f9389002b7460eb4bda3ace8de6680c07f5a3808
dDevusS <geckahilden@gmail.com>
2023-10-23 15:12:26 -0400

    Fixed methods of GameField. Now it's work.

--- src/gameEntity/GameField.java
+++ src/gameEntity/GameField.java
@@ -8,29 +8,8 @@ public class GameField {
 	private HashMap<Coordinate, Cell> gameField = new HashMap<>();
 
 	public GameField(int columns, int rows, int quantityOfBombs) {
-		Random random = new Random();
-		int counterBombs = 0;
-
-		while (counterBombs < quantityOfBombs) {
-			int column = random.nextInt(1, columns);
-			int row = random.nextInt(1 , rows);
-			Coordinate randomCoordinate = new Coordinate(column, row);
-
-			if (gameField.get(randomCoordinate) == null || !gameField.get(randomCoordinate).isBomb()) {
-				gameField.put(randomCoordinate, new Cell(column, row, 9));
-				counterBombs++;
-
-				for (int y = 1; y >= -1; y--) {
-					for (int x = -1; x <= 1; x++) {
-						if (gameField.get(new Coordinate(column + y, row + x)) == null) {
-							gameField.put(new Coordinate(column + y, row + x), new Cell(column + y, row + x, 1));
-						} else if (!gameField.get(new Coordinate(column + y, row + x)).isBomb()) {
-							gameField.get(new Coordinate(column + y, row + x)).plusStep();
-						}
-					}
-				}
-			}
-		}
+		this.createClearField(columns, rows);
+		this.createBombs(columns, rows, quantityOfBombs);
 	}
 
 	public HashMap<Coordinate, Cell> getGameField() {
@@ -69,7 +48,7 @@ public class GameField {
 
 				for (int y = 1; y >= -1; y--) {
 					for (int x = -1; x <= 1; x++) {
-						if (!gameField.get(new Coordinate(column + y, row + x)).isBomb()) {
+						if (gameField.get(new Coordinate(column + y, row + x)) != null && !gameField.get(new Coordinate(column + y, row + x)).isBomb()) {
 							gameField.get(new Coordinate(column + y, row + x)).plusStep();
 						}
 					}
