620d9cef069e89860ee3d488ae86da2331478760
dDevusS <geckahilden@gmail.com>
2023-11-01 21:07:23 -0400

    Created class ProcessedCommand

---
+++ src/gameProcessing/processingUserCommand/Command.java
@@ -0,0 +1,5 @@
+package gameProcessing.processingUserCommand;
+
+public enum Command {
+
+}
--- src/gameProcessing/processingUserCommand/ProcessedCommand.java
+++ src/gameProcessing/processingUserCommand/ProcessedCommand.java
@@ -1,5 +1,22 @@
 package gameProcessing.processingUserCommand;
 
-public class ProcessedCommand {
+import gameEntity.Coordinate;
 
+public class ProcessedCommand {
+	
+	private Command userCommand;
+	private Coordinate imputCoordinate;
+	
+	ProcessedCommand(Command userCommand, Coordinate inputCoordinate) {
+		this.userCommand = userCommand;
+		this.imputCoordinate = inputCoordinate;
+	}
+
+	public Command getUserCommand() {
+		return userCommand;
+	}
+
+	public Coordinate getImputCoordinate() {
+		return imputCoordinate;
+	}
 }
